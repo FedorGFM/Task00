16737a989e6455309259b6b4a2af7801b9491c90
Fedor <4ips228@gmail.com>
2022-12-22 04:43:08 +0700

    HomeWorkTask06

---
+++ HomeWorkTask06/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая на вход
+// принимает число и выдаёт, является ли число чётным
+// (делится ли оно на два без остатка).
+
+Console.Write("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.Write("Yes");
+
+else if (number % 2 > 0) Console.Write("No");
+
+else if (number % 2 < 0) Console.Write("No");
