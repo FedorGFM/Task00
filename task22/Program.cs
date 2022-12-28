33d606781cc0f8b0e23608b7efe51bae21ff1a36
Fedor <4ips228@gmail.com>
2022-12-29 01:54:54 +0700

    внесли правки в task22

--- task22/Program.cs
+++ task22/Program.cs
@@ -16,7 +16,7 @@ void TableSquare(int num)
     int count = 1;
     while (count <= num)
     {
-        Console.WriteLine($"{count} -> " + Math.Pow(count, 2)); 
+        Console.WriteLine($"{count, 4} -> {Math.Pow(count, 2), 4}"); 
         //Console.WriteLine($"{count}\t" + Math.Pow(count, 2)); можно так через табуляцию
         count ++;
     }
