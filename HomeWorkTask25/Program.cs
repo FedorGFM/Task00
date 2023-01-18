b5286aaf610087f8b1653b8fab57e49f040a8e82
Fedor <4ips228@gmail.com>
2023-01-18 21:49:10 +0700

    Изменения в HWtask25

--- HomeWorkTask25/Program.cs
+++ HomeWorkTask25/Program.cs
@@ -4,21 +4,31 @@
 // Например
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
- 
-Console.Write("Введите натуральное число A: ");
+
+Console.Write("Введите число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите натуральное число B: ");
+Console.Write("Введите натуральную степень B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-int num = SumDigit(numberA, numberB);
-Console.WriteLine($"Число {numberA} в степени {numberB} = {num}");
-
-int SumDigit(int numberA, int numberB)
+if (numberB <= 0)
 {
-    int result = 1;
-    for(int i = 1; i <= numberB; i++)
+    Console.WriteLine($"Ошибка {numberB} не натуральная степень");
+}
+
+else
+
+{
+    int num = SumDigit(numberA, numberB);
+
+    Console.WriteLine($"Число {numberA} в степени {numberB} = {num}");
+
+    int SumDigit(int numberA, int numberB)
     {
-        result = result * numberA;
+        int result = 1;
+        for (int i = 1; i <= numberB; i++)
+        {
+            result = result * numberA;
+        }
+        return result;
     }
-    return result;
 }
